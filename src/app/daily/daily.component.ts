4267b5682359eaf2798df1e4348412346f874b0d
Merkury32 <pjozefiak32@gmail.com>
2021-04-16 15:35:25 +0200

    add weather data map

--- src/app/daily/daily.component.ts
+++ src/app/daily/daily.component.ts
@@ -9,9 +9,15 @@ import { weatherService } from '../service/weather.service';
 export class DailyComponent implements OnInit {
   constructor(private weatherService: weatherService) {}
 
+  actualTemperature: number;
+  feelsTemperature: number;
+
   ngOnInit() {
     this.weatherService.getData().subscribe((data) => {
       console.log(data);
+
+      this.actualTemperature = data[4].temp;
+      this.feelsTemperature = data[4].feels_like;
     });
   }
 }
--- src/app/service/weather.service.ts
+++ src/app/service/weather.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -9,10 +10,20 @@ export class weatherService {
   constructor(private http: HttpClient) {}
 
   getData() {
-    let response = this.http.get(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=52.0887&lon=17.01506&units=metric&exclude=daily,hourly,weekly,minutely&appid=${environment.apiKey}`
-    );
-
-    return response;
+    return this.http
+      .get(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=52.0887&lon=17.01506&units=metric&exclude=daily,hourly,weekly,minutely&appid=${environment.apiKey}`
+      )
+      .pipe(
+        map((responseData) => {
+          const postsArray = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
+      );
   }
 }
