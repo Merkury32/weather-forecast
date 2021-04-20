c061d88635e576aad8818be922185afdb57b4f1e
Merkury32 <pjozefiak32@gmail.com>
2021-04-20 20:07:01 +0200

    add background svg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,16 @@ import { DailyComponent } from './daily/daily.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { HttpClientModule } from '@angular/common/http';
+import { DayBackgroundComponent } from './svg/day-background/day-background.component';
+import { NightBackgroundComponent } from './svg/night-background/night-background.component';
 
 @NgModule({
-  declarations: [AppComponent, DailyComponent],
+  declarations: [
+    AppComponent,
+    DailyComponent,
+    DayBackgroundComponent,
+    NightBackgroundComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/svg/day-background/day-background.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-day-background',
+  templateUrl: './day-background.component.html',
+  styleUrls: ['./day-background.component.scss']
+})
+export class DayBackgroundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
