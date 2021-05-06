999a669ea87eabfe0f995474c7de0b5fed285b6f
Merkury32 <pjozefiak32@gmail.com>
2021-05-06 12:52:35 +0200

    delete google geocode api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
   weatherApiKey: 'bc1f103d0825160fc4ecba0adc35009a',
-  geocodeApiKey: 'AIzaSyCG3bgPHumHKgwzgE9Ey7MLMoDyw0CYItc',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,4 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   weatherApiKey: 'bc1f103d0825160fc4ecba0adc35009a',
-  geocodeApiKey: 'AIzaSyCG3bgPHumHKgwzgE9Ey7MLMoDyw0CYItc',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
